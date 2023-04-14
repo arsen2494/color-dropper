20accab9cb83c72861641b6085bb49561e6b0cdb
Arsen Adamyan <arsenadamyan2494@gmail.com>
2023-04-14 11:43:08 +0400

    Color dropper app

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,4 @@
-export const rgbToHex = (red: number, green: number, blue: number): string => `#${_componentToHex(red)}${_componentToHex(green)}${_componentToHex(green)}`;
+export const rgbToHex = (red: number, green: number, blue: number): string => `#${_componentToHex(red)}${_componentToHex(green)}${_componentToHex(blue)}`;
 
 function _componentToHex(c: number) {
     const hex = c?.toString(16);
